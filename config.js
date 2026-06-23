7e08c15a7cce634524e63ad430aa70271233a237
Jocy John <45987166+jocyjohn47@users.noreply.github.com>
2026-06-23 19:33:24 +0400

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ window.LARK_REPAIR_KSA_URL = window.LARK_REPAIR_KSA_URL || "";
 
 // Notes shown on pages for all users/admin.
 // Edit text inside quotes. Leave empty "" to hide.
-window.AERONEX_SPARE_ORDER_NOTE = '<span style="color:red;font-weight:bold;">Once the order is applied,please send us the work order number by email.We will then send you the invoice and the order details</span>';
+window.AERONEX_SPARE_ORDER_NOTE = "Once the order is applied,please send us the work order number by email.We will then send you the invoice and the order details";
 window.AERONEX_REPAIR_CASE_NOTE = "For the repair case,please provide all the required details and documents as per DJI standards.Upload the files and share the link in the form.Once we verify the details,we will reply with the further procedure";
 
 window.AERONEX_R2_PUBLIC_URL = "https://pub-814d6055e13444a8adb21c87d7c724bc.r2.dev";
